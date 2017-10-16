bd5279957b864df08421c2f05e31ffb5f9191b61
gradiuscypher <gradiuscypher@gmail.com>
2017-10-15 23:12:00 -0700

    first steps to getting this working

---
+++ webhookbot.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mmcdole/gofeed"
+)
+
+func main() {
+	feedParser := gofeed.NewParser()
+	feed, _ := feedParser.ParseURL("https://threatpost.com/feed/")
+	fmt.Println(feed.Title)
+	fmt.Println(feed.UpdatedParsed)
+
+	for _, item := range feed.Items {
+		fmt.Println(item.Title)
+		fmt.Println(item.Description)
+	}
+}
